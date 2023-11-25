c01c202194e4dc14be3ec60c7fcddb737df919f0
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-26 08:47:09 +0900

    :sparkles: feat: 게임 실행에 필요한 메세지와 조건들 상수화

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,18 @@
-const USER_COMMAND = Object.freeze({});
+const USER_COMMAND = Object.freeze({
+  retry: 1,
+  end: 2,
+});
 
-export default { USER_COMMAND };
+const GAME_RESULT = Object.freeze({
+  ball: '볼',
+  strike: '스트라이크',
+  nothing: '낫싱',
+});
+
+const NUMBER_CONDITION = Object.freeze({
+  minRange: 1,
+  maxRange: 9,
+  validLength: 3,
+});
+
+export default { USER_COMMAND, GAME_RESULT, NUMBER_CONDITION };
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,11 @@
-const INPUT_MESSAGE = Object.freeze({});
+const INPUT_MESSAGE = Object.freeze({
+  requireUserNumber: '숫자를 입력해주세요 : ',
+  requireUserCommand: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+});
 
-const OUTPUT_MESSAGE = Object.freeze({});
+const OUTPUT_MESSAGE = Object.freeze({
+  startGame: '숫자 야구 게임을 시작합니다.',
+  completeGame: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+});
 
 export default { INPUT_MESSAGE, OUTPUT_MESSAGE };

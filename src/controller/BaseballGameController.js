547ae99d2016737490d6761b3e0e2be8eabc0462
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-26 11:11:42 +0900

    :sparkles: feat: 컨트롤러에서 게임의 전반적인 실행 로직 구현 입력값을 호출한 후 검증 메서드에 전달함, 유효한 값만 도메인에 전달함, 게임 종료조건과 명령어에 따른 분기처리

--- src/controller/BaseballGameController.js
+++ src/controller/BaseballGameController.js
@@ -1,5 +1,41 @@
+import BaseballGame from '../domain/BaseballGame.js';
+import InputValidator from '../domain/validators/InputValidator.js';
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+import { USER_COMMAND } from '../constants/constants.js';
+
 class BaseballGameController {
-  constructor() {}
+  #baseballGame;
+  #isCompleteCondition;
+
+  constructor() {
+    OutputView.printStartMessage();
+  }
+
+  async startGame() {
+    this.#baseballGame = new BaseballGame();
+    this.#isCompleteCondition = this.#baseballGame.getCompleteCondition();
+
+    while (!this.#isCompleteCondition) {
+      const userNumber = await InputView.readUserNumber();
+
+      InputValidator.validateUserNumber(userNumber);
+
+      const { ball, strike } = this.#baseballGame.getCompareResult(userNumber);
+
+      OutputView.printCompareResult(ball, strike);
+
+      this.#isCompleteCondition = this.#baseballGame.getCompleteCondition();
+    }
+
+    OutputView.printCompleteMessage();
+
+    const userCommand = await InputView.readUserCommand();
+
+    InputValidator.validateUserCommand(userCommand);
+
+    if (userCommand === USER_COMMAND.retry) return this.startGame();
+  }
 }
 
 export default BaseballGameController;

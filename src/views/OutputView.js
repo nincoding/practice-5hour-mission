9401f9cc052aa5c594fd2c50717847ec3766ae8f
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-26 11:01:44 +0900

    :sparkles: feat: 숫자 비교 결과 출력 기능 추가 볼과 스트라이크의 값을 전달받아 분기처리 후 출력한다

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,14 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 import { OUTPUT_MESSAGE } from '../constants/messages.js';
+import { GAME_RESULT } from '../constants/constants.js';
 
 const OutputView = {
   printStartMessage() {
     Console.print(OUTPUT_MESSAGE.startGame);
   },
 
-  // todo: 볼, 스트라이크, 낫싱 분기
-  printCompareResult(gameResult) {
-    Console.print();
+  printCompareResult(ball, strike) {
+    if (ball === 0 && strike === 0) return Console.print(GAME_RESULT.nothing);
+    if (ball > 0 && strike === 0) return Console.print(`${ball}${GAME_RESULT.ball}`);
+    if (ball === 0 && strike > 0) return Console.print(`${strike}${GAME_RESULT.strike}`);
+    if (ball > 0 && strike > 0)
+      return Console.print(`${ball}${GAME_RESULT.ball} ${strike}${GAME_RESULT.strike}`);
   },
 
   printCompleteMessage() {

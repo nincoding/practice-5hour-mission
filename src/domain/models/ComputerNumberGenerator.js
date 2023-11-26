a275f71f035dd10d14fd7bac6ec4f3fb342bf6c5
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-26 11:03:36 +0900

    :sparkles: feat: 컴퓨터의 랜덤 숫자 3자리 생성기능 추가

--- src/domain/models/ComputerNumberGenerator.js
+++ src/domain/models/ComputerNumberGenerator.js
@@ -1,5 +1,31 @@
+import { Random } from '@woowacourse/mission-utils';
+import { NUMBER_CONDITION } from '../../constants/constants.js';
+
 class ComputerNumberGenerator {
-  constructor() {}
+  #computerNumber;
+
+  constructor() {
+    this.#computerNumber = this.#createRandomNumber();
+  }
+
+  getComputerNumber() {
+    return this.#computerNumber;
+  }
+
+  #createRandomNumber() {
+    const uniqueSetNumbers = new Set();
+
+    while (uniqueSetNumbers.size < NUMBER_CONDITION.validLength) {
+      const randomNumber = Random.pickNumberInRange(
+        NUMBER_CONDITION.minRange,
+        NUMBER_CONDITION.maxRange
+      );
+
+      uniqueSetNumbers.add(randomNumber);
+    }
+
+    return parseInt([...uniqueSetNumbers].join(''), 10);
+  }
 }
 
 export default ComputerNumberGenerator;

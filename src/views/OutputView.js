caaa98a01830aafd72b12152e8e18a45d3fa2a6e
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-26 09:02:47 +0900

    :sparkles: feat: 출력뷰 메세지 출력 기능 구현 게임 시작 문구를 출력한다, 각 숫자를 비교한 결과를 출력한다, 게임 종료 문구를 출력한다

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,3 +1,19 @@
-const OutputView = {};
+import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGE } from '../constants/messages.js';
+
+const OutputView = {
+  printStartMessage() {
+    Console.print(OUTPUT_MESSAGE.startGame);
+  },
+
+  // todo: 볼, 스트라이크, 낫싱 분기
+  printCompareResult(gameResult) {
+    Console.print();
+  },
+
+  printCompleteMessage() {
+    Console.print(OUTPUT_MESSAGE.completeGame);
+  },
+};
 
 export default OutputView;

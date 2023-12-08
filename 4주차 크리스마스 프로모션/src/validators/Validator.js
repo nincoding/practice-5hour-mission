8bfa5698e8facccc068b1610fd7b79ebfd4078c3
한수지 <fatholic5@gmail.com>
2023-12-08 15:31:26 +0900

    :sparkles: feat: 방문날짜 유효성 검증 기능 추가 빈문자열이 아닌지 검증, 숫자인지 검증, 1이상 31이하의 범위에 해당하는지 검증 예외일 경우 에러메세지를 가진 커스텀에러를 내보냄

--- 4주차 크리스마스 프로모션/src/validators/Validator.js
+++ 4주차 크리스마스 프로모션/src/validators/Validator.js
@@ -1,6 +1,24 @@
 import CustomError from '../errors/CustomError.js';
 import Validation from './Validation.js';
 
-const Validator = {};
+const Validator = {
+  validateDate(input) {
+    const validateEmptyString = Validation.isEmptyString(input);
+    const validateInteger = Validation.isValidInteger(input);
+    const validateRange = Validation.isValidRange(input, 1, 31);
+
+    if (validateEmptyString || !validateInteger || !validateRange) {
+      throw new CustomError('유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
+  },
+
+  validateOrder(input) {
+    const validateEmptyString = Validation.isEmptyString(input);
+
+    if (validateEmptyString) {
+      throw new CustomError('유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+  },
+};
 
 export default Validator;

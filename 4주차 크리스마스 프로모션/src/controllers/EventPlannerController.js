870a7d011639025e64305c8c365d5d7cddb2b4b4
한수지 <fatholic5@gmail.com>
2023-12-08 17:51:48 +0900

    :sparkles: feat: 컨트롤러 방문날짜와 주문 핸들링 기능 추가

--- 4주차 크리스마스 프로모션/src/controllers/EventPlannerController.js
+++ 4주차 크리스마스 프로모션/src/controllers/EventPlannerController.js
@@ -1,7 +1,39 @@
-class EventPlannerController {
-  constructor() {}
+import OutputView from '../views/OutputView.js';
+import InputView from '../views/InputView.js';
 
-  async start() {}
+class EventPlannerController {
+  constructor() {
+    OutputView.printStart();
+  }
+
+  async start() {
+    const date = await this.handleVisitDate();
+    const order = await this.handleOrder();
+  }
+
+  async handleVisitDate() {
+    try {
+      const visitDate = await InputView.readDate();
+
+      return Number(visitDate);
+    } catch ({ message }) {
+      OutputView.printError(message);
+
+      await this.handleVisitDate();
+    }
+  }
+
+  async handleOrder() {
+    try {
+      const order = await InputView.readOrder();
+
+      return order;
+    } catch ({ message }) {
+      OutputView.printError(message);
+
+      await this.handleOrder();
+    }
+  }
 }
 
 export default EventPlannerController;

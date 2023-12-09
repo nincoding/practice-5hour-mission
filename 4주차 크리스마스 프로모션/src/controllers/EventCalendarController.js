f7faefdfab559aabd936b17afee471a06d360666
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 17:04:16 +0900

    :sparkles: feat: 시작문구 추가

--- 4주차 크리스마스 프로모션/src/controllers/EventCalendarController.js
+++ 4주차 크리스마스 프로모션/src/controllers/EventCalendarController.js
@@ -1,5 +1,10 @@
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+
 class EventCalendarController {
-  constructor() {}
+  constructor() {
+    OutputView.printStart();
+  }
 
   async start() {}
 }

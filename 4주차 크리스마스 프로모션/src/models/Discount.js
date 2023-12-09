7508df16184b15ce81862156fb1dc36afbf209c9
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 14:36:05 +0900

    :sparkles: feat: 크리스마스 디데이 할인 기능 추가

--- 4주차 크리스마스 프로모션/src/models/Discount.js
+++ 4주차 크리스마스 프로모션/src/models/Discount.js
@@ -1,5 +1,18 @@
-class Discount {
-  constructor() {}
-}
+import { CALENDAR } from '../constants/constant.js';
+
+const Discount = {
+  christmasDayDiscount(date) {
+    const isEventCondition = date <= CALENDAR.christmas;
+    const discountAmount = 1000 + (date - 1) * 100;
+
+    return isEventCondition ? discountAmount : null;
+  },
+
+  weekdayDiscount(date, order) {},
+
+  weekendDiscount(date, order) {},
+
+  specialDiscount(date) {},
+};
 
 export default Discount;

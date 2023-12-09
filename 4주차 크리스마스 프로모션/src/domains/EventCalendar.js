42fecfaba2846f41e60d0c9b81da1f9f44473c1c
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 15:34:07 +0900

    :sparkles: feat: 총 주문금액 계산 기능 추가

--- 4주차 크리스마스 프로모션/src/domains/EventCalendar.js
+++ 4주차 크리스마스 프로모션/src/domains/EventCalendar.js
@@ -1,5 +1,34 @@
+import { CATEGORY, MENU } from '../constants/constant.js';
+
 class EventCalendar {
-  constructor() {}
+  #totalOrderAmount;
+
+  constructor(date, order) {
+    this.date = date;
+    this.order = order;
+    this.#totalOrderAmount = this.#calcTotalOrderAmount();
+  }
+
+  getTotalOrderAmount() {
+    return this.#calcTotalOrderAmount;
+  }
+
+  #calcTotalOrderAmount() {
+    const allMenus = Object.values(MENU).flatMap((category) => {
+      return category.map((menu) => ({ [menu.menu]: menu.prize }));
+    });
+
+    const totalOrderAmount = this.order.reduce((total, { menu, count }) => {
+      return total + allMenus.find(({ [menu]: price }) => price)[menu] * count;
+    }, 0);
+
+    return totalOrderAmount;
+  }
 }
 
 export default EventCalendar;
+
+/*
+const order = [{ menu: '티본스테이크', count: 1 }];
+new EventCalendar(3, order).calcTotalOrderAmount();
+*/

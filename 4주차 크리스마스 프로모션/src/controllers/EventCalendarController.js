2ee93b5d8b2ce33b39b676d8f5601ca2f0dbd22e
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 17:08:08 +0900

    :sparkles: feat: 입력값 컨트롤 기능 추가

--- 4주차 크리스마스 프로모션/src/controllers/EventCalendarController.js
+++ 4주차 크리스마스 프로모션/src/controllers/EventCalendarController.js
@@ -6,7 +6,36 @@ class EventCalendarController {
     OutputView.printStart();
   }
 
-  async start() {}
+  async start() {
+    const date = await this.#handleDate();
+    const order = await this.#handleOrder();
+  }
+
+  async #handleDate() {
+    try {
+      const date = await InputView.readDate();
+
+      return date;
+    } catch ({ message }) {
+      OutputView.printError(message);
+
+      return await this.#handleDate();
+    }
+  }
+
+  async #handleOrder() {
+    try {
+      const order = await InputView.readOrder();
+
+      return order;
+    } catch ({ message }) {
+      OutputView.printError(message);
+
+      return await this.#handleOrder();
+    }
+  }
+
+  #printResult() {}
 }
 
 export default EventCalendarController;

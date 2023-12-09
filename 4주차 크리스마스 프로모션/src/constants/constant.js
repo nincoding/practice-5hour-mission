c74cb57681dcb04cd2b77e8606aa09eb1e4a40ec
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 14:06:15 +0900

    :sparkles: feat: 메뉴 상수화

--- 4주차 크리스마스 프로모션/src/constants/constant.js
+++ 4주차 크리스마스 프로모션/src/constants/constant.js
@@ -1,3 +1,70 @@
-const MENU = Object.freeze({});
+const CATEGORY = Object.freeze({
+  appetizer: '애피타이저',
+  main: '메인',
+  dessert: '디저트',
+  drink: '음료',
+});
 
-export { MENU };
+const MENU = Object.freeze({
+  [CATEGORY.appetizer]: [
+    {
+      menu: '양송이수프',
+      prize: 6_000,
+    },
+    {
+      menu: '타파스',
+      prize: 5_500,
+    },
+    {
+      menu: '시저샐러드',
+      prize: 8_000,
+    },
+  ],
+
+  [CATEGORY.main]: [
+    {
+      menu: '티본스테이크',
+      prize: 55_000,
+    },
+    {
+      menu: '바비큐립',
+      prize: 54_000,
+    },
+    {
+      menu: '해산물파스타',
+      prize: 35_000,
+    },
+    {
+      menu: '크리스마스파스타',
+      prize: 25_000,
+    },
+  ],
+
+  [CATEGORY.dessert]: [
+    {
+      menu: '초코케이크',
+      prize: 15_000,
+    },
+    {
+      menu: '아이스크림',
+      prize: 5_000,
+    },
+  ],
+
+  [CATEGORY.drink]: [
+    {
+      menu: '제로콜라',
+      prize: 3_000,
+    },
+    {
+      menu: '레드와인',
+      prize: 60_000,
+    },
+    {
+      menu: '샴페인',
+      prize: 25_000,
+    },
+  ],
+});
+
+export { MENU, CATEGORY };

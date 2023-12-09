ff0ecdb8a188ecd879d80cdf2dc929e5a857831f
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 14:20:23 +0900

    :sparkles: feat: 이벤트 모델 기능 추가 증정 이벤트 기능 추가, 전체주문금액이 12만원이상일 경우 증정상품을 1개 추가하여 객체로 반환, 배지 이벤트 기능 추가, 전체혜택금액에 따라 각각 다른 배지를 반환

--- 4주차 크리스마스 프로모션/src/models/Event.js
+++ 4주차 크리스마스 프로모션/src/models/Event.js
@@ -1,5 +1,18 @@
-class Event {
-  constructor() {}
-}
+import { MENU, CATEGORY } from '../constants/constant.js';
+
+const Event = {
+  present(totalAmount) {
+    const desiredMenu = '샴페인';
+    const presentItem = MENU[CATEGORY.drink].find((item) => item.menu === desiredMenu);
+
+    return totalAmount >= 120_000 ? { [presentItem.menu]: 1 } : {};
+  },
+
+  badge(totalBenefitAmount) {
+    if (totalBenefitAmount >= 20_000) return '산타';
+    if (totalBenefitAmount >= 10_000) return '트리';
+    if (totalBenefitAmount >= 5_000) return '별';
+  },
+};
 
 export default Event;

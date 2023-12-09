4bbe6ac55994bf28ec8e13102b67dca437b4b985
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 14:40:37 +0900

    :sparkles: feat: 특별 할인 기능 추가 이벤트 달력에 방문날짜가 있으면 1,000원 할인한다

--- 4주차 크리스마스 프로모션/src/models/Discount.js
+++ 4주차 크리스마스 프로모션/src/models/Discount.js
@@ -3,16 +3,22 @@ import { CALENDAR } from '../constants/constant.js';
 const Discount = {
   christmasDayDiscount(date) {
     const isEventCondition = date <= CALENDAR.christmas;
-    const discountAmount = 1000 + (date - 1) * 100;
+    const calcDiscount = 1000 + (date - 1) * 100;
+    const discountAmount = isEventCondition ? calcDiscount : null;
 
-    return isEventCondition ? discountAmount : null;
+    return discountAmount;
   },
 
   weekdayDiscount(date, order) {},
 
   weekendDiscount(date, order) {},
 
-  specialDiscount(date) {},
+  specialDiscount(date) {
+    const isEventCondition = CALENDAR.special.includes(date);
+    const discountAmount = isEventCondition ? 1000 : null;
+
+    return discountAmount;
+  },
 };
 
 export default Discount;

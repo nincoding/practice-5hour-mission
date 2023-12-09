557b31410c26a2bd9b45fafea46003da6650e612
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-09 16:07:26 +0900

    :sparkles: feat: 혜택 적용 내역 계산기능 추가, 증정이벤트 계산기능 추가

--- 4주차 크리스마스 프로모션/src/domains/EventCalendar.js
+++ 4주차 크리스마스 프로모션/src/domains/EventCalendar.js
@@ -1,3 +1,5 @@
+import Discount from '../models/Discount.js';
+import Event from '../models/Event.js';
 import { CATEGORY, MENU } from '../constants/constant.js';
 
 class EventCalendar {
@@ -13,6 +15,17 @@ class EventCalendar {
     return this.#calcTotalOrderAmount;
   }
 
+  getBenefitsHistory() {
+    const isEventCondition = this.#totalOrderAmount >= 10_000;
+    const benefitsHistory = isEventCondition ? this.#calcBenefitsHistory() : null;
+
+    return benefitsHistory;
+  }
+
+  getPresent() {
+    return Event.present(this.#totalOrderAmount);
+  }
+
   #calcTotalOrderAmount() {
     const allMenus = Object.values(MENU).flatMap((category) => {
       return category.map((menu) => ({ [menu.menu]: menu.prize }));
@@ -24,11 +37,29 @@ class EventCalendar {
 
     return totalOrderAmount;
   }
+
+  #calcBenefitsHistory() {
+    const benefitsHistory = [];
+    const addEventToHistory = (eventName, discountAmount) => {
+      if (discountAmount !== null && discountAmount !== 0) {
+        benefitsHistory.push({ [eventName]: discountAmount });
+      }
+    };
+
+    addEventToHistory('크리스마스 디데이 할인', Discount.christmasDayDiscount(this.date));
+    addEventToHistory('평일 할인', Discount.weekdayDiscount(this.date, this.order));
+    addEventToHistory('주말 할인', Discount.weekendDiscount(this.date, this.order));
+    addEventToHistory('특별 할인', Discount.specialDiscount(this.date));
+    addEventToHistory('증정 이벤트', Discount.presentDiscount(this.getPresent()));
+
+    return benefitsHistory;
+  }
 }
 
 export default EventCalendar;
 
 /*
-const order = [{ menu: '티본스테이크', count: 1 }];
-new EventCalendar(3, order).calcTotalOrderAmount();
+const order = [{ menu: '티본스테이크', count: 5 }];
+new EventCalendar(2, order).getBenefitsHistory();
+new EventCalendar(3, order).getPresent();
 */

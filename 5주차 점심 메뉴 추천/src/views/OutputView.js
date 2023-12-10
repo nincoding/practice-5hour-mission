872c54ba40d7cfcd0097e1b261bd02a5ae70986a
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-10 19:31:02 +0900

    :sparkles: feat: 결과 출력 포맷기능 추가

--- 5주차 점심 메뉴 추천/src/views/OutputView.js
+++ 5주차 점심 메뉴 추천/src/views/OutputView.js
@@ -9,6 +9,27 @@ const OutputView = {
   printStart() {
     Console.print(OUTPUT_MESSAGE.printStart);
   },
+
+  printResult(result) {
+    Console.print(OUTPUT_MESSAGE.printRecommendResult);
+
+    for (const row of result) {
+      const header = Object.keys(row)[0];
+      const values = row[header];
+
+      const formattedRow = this.formatRow(header, values);
+
+      Console.print(formattedRow);
+    }
+
+    Console.print(OUTPUT_MESSAGE.printEnd);
+  },
+
+  formatRow(header, values) {
+    const headerRow = `[ ${header} | ${values.join(' | ')} ]`;
+
+    return headerRow;
+  },
 };
 
 export default OutputView;

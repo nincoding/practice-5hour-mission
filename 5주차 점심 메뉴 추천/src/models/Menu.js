ea1168c30f1e951c7bd622d265fed110bd8a3251
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-12-10 19:31:56 +0900

    :recycle: refactor: 추천 인덱스 수정

--- 5주차 점심 메뉴 추천/src/models/Menu.js
+++ 5주차 점심 메뉴 추천/src/models/Menu.js
@@ -14,10 +14,10 @@ class Menu {
 
   #recommendMenu(category) {
     const categoryMenus = MENU[category].split(', ');
-    const categoryMenusIndex = categoryMenus.map((_, index) => index);
+    const categoryMenusIndex = categoryMenus.map((_, index) => index + 1);
     const menuIndex = Random.shuffle(categoryMenusIndex)[0];
 
-    this.#menu = categoryMenus[menuIndex];
+    this.#menu = categoryMenus[menuIndex - 1];
   }
 }
 
